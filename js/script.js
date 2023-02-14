756482b0a02a9e19e91ad1762914552f598ede7f
MuseSheng <muse.sheng@gmail.com>
2023-02-14 21:40:38 +0800

    2.3.3

--- js/script.js
+++ js/script.js
@@ -589,7 +589,7 @@ function connectMetaMask(){
 
         }else{
             $("div[class = 'learnConnect'] > h2").text("Metamask is not installed");
-            $(".walletImg").css("background-image", "url(../img/metamaskWallet.png)");
+            $(".walletImg").css("background-image", "url(../CryptoMuse/img/metamaskWallet.png)");
             $(".learnConnectBtn").attr({
                 onclick: "window.open('https://metamask.io/download/');"
             });
