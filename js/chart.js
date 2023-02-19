d6aa35c5e5a6d5cef3bd47010b51e57f20d931e8
MuseSheng <muse.sheng@gmail.com>
2023-02-19 23:13:51 +0800

    2.5.5

--- js/chart.js
+++ js/chart.js
@@ -1,91 +1,9 @@
-let ctx = document.getElementById('swapChart').getContext('2d');
 
-let gradient = ctx.createLinearGradient(0, 0, 0, 400);
-    gradient.addColorStop(0, 'rgba(250,174,50,1)');   
-    gradient.addColorStop(1, 'rgba(250,174,50,0.2)');
-
-let chart = new Chart(ctx, {
-  
-	type: 'line',
-  	data: {
-    	labels: [],
-    	datasets:[
-    		{			
-				label: 'Dogecoin (DOGE)',
-				data: [],
-				borderColor: 'rgba(247,147,26,1)',
-				backgroundColor: gradient,
-				//borderJoinStyle: 'round',
-				//borderCapStyle: 'round',
-				borderWidth: 3,
-				pointRadius: 0,
-				pointHitRadius: 10,
-				fill: true
-			}
-		]
-	},
-  	options: {
-		title: {
-			display: false,
-			family: "'Montserrat', 'sans-serif'",
-			text: 'Dogecoin',
-			fontSize: 28
-		},
-		scales: {
-			xAxes: [{
-				display: false,
-				gridLines: false
-			}],
-			yAxes: [{
-				display: true,
-				gridLines: {}
-			}],
-			/* y: {
-        		beginAtZero: false
-      		} */
-			x: {
-				title: {
-					display: true,
-					text: "Last 3 days"
-				}
-			}
-		},
-		legend: {
-			display: false
-		},
-		layout: {
-			padding: {
-				left: 0,
-				right: 0,
-				top: 0,
-				bottom: 0
-			}
-		},
-
-		tooltips: {
-			/* callbacks: {
-			//This removes the tooltip title
-			title: function() {}
-			}, */
-			//this removes legend color
-			displayColors: false,
-			yPadding: 20,
-			xPadding: 10,
-			position: 'nearest',
-			caretSize: 10,
-			backgroundColor: 'rgba(0, 0, 0, 0.5)',
-			bodyFontSize: 18,
-			bodyFontColor: 'rgba(237, 234, 243, 1)' 
-    	},
-		aspectRatio: 1.2,
-		maintainAspectRatio: true
-  	},
-});
 
 
 // Select Coin ID
 let coinId = "bitcoin";
-
+let labelName = "Bitcoin";
 function getCoin(){
     coinId = event.target.id;
     console.log("ID: " + coinId);
@@ -99,7 +17,6 @@ function getCoin(){
 	//let metamaskImgUrl = "CryptoMuse/img/metamaskWallet.png";
     $(".swapCoinLogo").css("background-image", `url(../CryptoMuse/img/swap/${newText}.png)`);
 	$(".coinImg_1").css("background-image", `url(../CryptoMuse/img/swap/${newText}.png)`);
-
 
     // Select Coin ID price
     let dataUrl= "https://api.coingecko.com/api/v3/coins/"+`${coinId}`+"/market_chart?vs_currency=usd&days=3"
@@ -170,4 +87,91 @@ axios.get("https://api.coingecko.com/api/v3/coins/"+`${coinId}`+"/market_chart?v
 })
 .catch(function (error) {
 console.log(error);
+});
+
+
+let ctx = document.getElementById('swapChart').getContext('2d');
+
+let gradient = ctx.createLinearGradient(0, 0, 0, 400);
+    gradient.addColorStop(0, 'rgba(250,174,50,1)');   
+    gradient.addColorStop(1, 'rgba(250,174,50,0.2)');
+
+
+
+let chart = new Chart(ctx, {
+  
+	type: 'line',
+  	data: {
+    	labels: [],
+    	datasets:[
+    		{			
+				label: false,
+				data: [],
+				borderColor: 'rgba(247, 147, 26, 0.8)',
+				backgroundColor: gradient,
+				//borderJoinStyle: 'round',
+				//borderCapStyle: 'round',
+				borderWidth: 3,
+				pointRadius: 0,
+				pointHitRadius: 10,
+				fill: true
+			}
+		]
+	},
+  	options: {
+		title: {
+			display: false,
+			family: "'Montserrat', 'sans-serif'",
+			text: 'Dogecoin',
+			fontSize: 28
+		},
+		scales: {
+			xAxes: [{
+				display: false,
+				gridLines: false
+			}],
+			yAxes: [{
+				display: true,
+				gridLines: {}
+			}],
+			/* y: {
+        		beginAtZero: false
+      		} */
+			x: {
+				title: {
+					display: true,
+					text: "Last 3 days"
+				}
+			}
+		},
+		legend: {
+			display: false
+		},
+		layout: {
+			padding: {
+				left: 0,
+				right: 0,
+				top: 0,
+				bottom: 0
+			}
+		},
+
+		tooltips: {
+			/* callbacks: {
+			//This removes the tooltip title
+			title: function() {}
+			}, */
+			//this removes legend color
+			displayColors: false,
+			yPadding: 20,
+			xPadding: 10,
+			position: 'nearest',
+			caretSize: 10,
+			backgroundColor: 'rgba(0, 0, 0, 0.5)',
+			bodyFontSize: 18,
+			bodyFontColor: 'rgba(237, 234, 243, 1)' 
+    	},
+		aspectRatio: 1.2,
+		maintainAspectRatio: true
+  	},
 });
